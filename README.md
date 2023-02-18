e7e85cc603ad2d418839d9e4882d0c9d723072a9
Nathan Laha <nlaha@outlook.com>
2023-02-18 11:43:35 -0800

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# cc2023
-Crimson Code 2023 WSU Segfault
+# Crimson LMS
+
+This app illustrates how to use [Passport](https://www.passportjs.org/) with
+[Express](https://expressjs.com/) to sign users in via [Auth0](https://auth0.com/).
+Use this example as a starting point for your own web applications.
+
+## Quick Start
+
+Be sure to install the latest node lts version from https://nodejs.org/en/
+and then install yarn with the following commands
+
+```bash
+corepack enable
+corepack prepare yarn@stable --activate
+```
+
+To run this app, clone the repository and install dependencies using yarn:
+
+```bash
+$ git clone https://github.com/nlaha/cc2023.git
+$ cd cc2023
+$ yarn
+```
+
+Download the `.env` file from the discord (Nathan will provide)
+
+Start the server.
+
+```bash
+$ yarn start
+```
---
+++ server.js
@@ -0,0 +1,88 @@
+const express = require("express");
+const path = require("path");
+
+const expressSession = require("express-session");
+const passport = require("passport");
+const Auth0Strategy = require("passport-auth0");
+
+require("dotenv").config();
+
+const authRouter = require("./auth");
+
+const app = express();
+const port = process.env.PORT || "3000";
+
+var SQLiteStore = require("connect-sqlite3")(expressSession);
+
+var db = require("./db");
+
+const session = {
+  secret: process.env.SESSION_SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: false,
+  store: new SQLiteStore({ db: "sessions.db", dir: "./db" }),
+};
+
+if (app.get("env") === "production") {
+  // Serve secure cookies, requires HTTPS
+  session.cookie.secure = true;
+}
+
+app.use(express.static(path.join(__dirname, "client/dist")));
+
+app.use(expressSession(session));
+
+const strategy = new Auth0Strategy(
+  {
+    domain: process.env.AUTH0_DOMAIN,
+    clientID: process.env.AUTH0_CLIENT_ID,
+    clientSecret: process.env.AUTH0_CLIENT_SECRET,
+    callbackURL: process.env.AUTH0_CALLBACK_URL,
+  },
+  function (accessToken, refreshToken, extraParams, profile, done) {
+    /**
+     * Access tokens are used to authorize users to an API
+     * (resource server)
+     * accessToken is the token to call the Auth0 API
+     * or a secured third-party API
+     * extraParams.id_token has the JSON Web Token
+     * profile has all the information from the user
+     */
+    return done(null, profile);
+  }
+);
+
+passport.use(strategy);
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+app.use("/", authRouter);
+
+// These routes require the user to log in
+const secured = (req, res, next) => {
+  if (req.user) {
+    return next();
+  }
+  req.session.returnTo = req.originalUrl;
+  res.redirect("/login");
+};
+
+app.get("/user", secured, (req, res, next) => {
+  const { _raw, _json, ...userProfile } = req.user;
+  res.json(userProfile);
+});
+
+app.listen(port, () => {
+  /* eslint-disable no-console */
+  console.log(`Listening: http://localhost:${port}`);
+  /* eslint-enable no-console */
+});
